f63af1a0d2d70a2d972cc6542cc2625269f7e18a
VladSkliar <vlad.skliar@gmail.com>
2015-09-09 18:10:16 +0300

    Delete mydatabase info

--- minisite/settings/dev.py
+++ minisite/settings/dev.py
@@ -3,15 +3,3 @@ from common import *
 EMAIL_BACKEND = "djrill.mail.backends.djrill.DjrillBackend"
 
 DEBUG = True
-
-MANDRILL_API_KEY = "84r7I2irZA34F-bdxMPsYQ"
-
-DATABASES = {
-    'default': {
-             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-             'NAME': 'mydb',
-             'USER': 'vladskliar',
-             'PASSWORD': 'qlewnfrup12',
-             'HOST': '',
-             'PORT': '5432'}
-}

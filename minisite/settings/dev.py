ac670b721847734ba030a1e4a78886ac24a054e8
VladSkliar <vlad.skliar@gmail.com>
2015-09-09 17:49:33 +0300

    Add db in dev.py

--- minisite/settings/dev.py
+++ minisite/settings/dev.py
@@ -5,3 +5,13 @@ EMAIL_BACKEND = "djrill.mail.backends.djrill.DjrillBackend"
 DEBUG = True
 
 MANDRILL_API_KEY = "84r7I2irZA34F-bdxMPsYQ"
+
+DATABASES = {
+    'default': {
+             'ENGINE': 'django.db.backends.postgresql_psycopg2',
+             'NAME': 'mydb',
+             'USER': 'vladskliar',
+             'PASSWORD': 'qlewnfrup12',
+             'HOST': '',
+             'PORT': '5432'}
+}

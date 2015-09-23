45b26369bf50a24d97a0a6ef76e34e7984f33fc5
VladSkliar <vlad.skliar@gmail.com>
2015-09-23 13:35:49 +0300

    Fixed bug with media files

--- minisite/urls.py
+++ minisite/urls.py
@@ -25,8 +25,8 @@ urlpatterns += solid_i18n_patterns(
   url(r'^ckeditor/', include('ckeditor.urls')),
 
   )
-urlpatterns += static(settings.MEDIA_URL,
-                      document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns() + static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,

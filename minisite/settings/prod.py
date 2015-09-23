14de1509eb6b0df51a694e80f073cbafdb268421
VladSkliar <vlad.skliar@gmail.com>
2015-09-23 12:44:17 +0300

    Delete secret settings

--- minisite/settings/prod.py
+++ minisite/settings/prod.py
@@ -2,6 +2,8 @@ from dev import *
 
 import dj_database_url
 
+DEBUG = False
+
 DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -11,19 +13,3 @@ ALLOWED_HOSTS = ['skliar.herokuapp.com']
 STATIC_ROOT = 'staticfiles'
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-DISQUS_API_KEY = 'MTo8ccda8F7ewHnYwpscUG9Kt3A6r1yBBxdUM9vv6k1OGl9u6g6MitmHJCGeaXCv'
-
-DISQUS_WEBSITE_SHORTNAME = 'skliar'
-
-DATABASES = {
-    'default': {
-             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-             'NAME': 'd9hot5t75935ag',
-             'USER': 'javxneutvfsgve',
-             'PASSWORD': 'mDoxBDWreCXVyjLcx58_Djjhn1',
-             'HOST': 'ec2-54-235-162-144.compute-1.amazonaws.com',
-             'PORT': '5432'}
-}
-
-DEBUG = False
